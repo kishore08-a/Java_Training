2b8793282095707c86d87b6f08ee22968ab91067
Kishore K <kk08kishore@gmail.com>
2026-05-28 12:37:04 +0530

    commit changes

--- Sumofoddandeven.java
+++ Sumofoddandeven.java
@@ -1,16 +1,16 @@
 public class Sumofoddandeven{
     public static void main(String[] args) {
-        int eve=0;
-        int od=0;
+        int even=0;
+        int odd=0;
         for(int i=1;i<=10;i++){
             if(i%2==0){
-                eve+=i;
+                even+=i;
             }
             else{
-                od+=i;
+                odd+=i;
             }
-        System.out.println("Even sum: " + eve);
-        System.out.println("Odd sum: " + od);
+        System.out.println("Even sum: " + even);
+        System.out.println("Odd sum: " + odd);
         }
     }
     
